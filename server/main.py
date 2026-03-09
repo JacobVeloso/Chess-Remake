0cec2f6c4c899cebc271e96bf2684fca5e4e37c9
JacobVeloso <jacob.a.veloso25@gmail.com>
2026-03-09 19:50:20 +1100

    Set default port to 5000

--- server/main.py
+++ server/main.py
@@ -49,5 +49,5 @@ def move():
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
